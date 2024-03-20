e61bd02199a1055af5621bb1c1dbb4f8a9bc17dd
pwnafication <jogthong@gmail.com>
2024-03-20 08:20:33 -0400

    Changed project names

--- third_parties/test_push.py
+++ third_parties/test_push.py
@@ -1,1 +1,12 @@
-"Push! ! ! ! !"
+class MyClass:
+    def __init__(self, x):
+        self.x = x
+
+    def print_x(self):
+        print(self.x)
+
+# Creating an instance of MyClass
+obj = MyClass(5)
+
+# Calling a method on the instance
+obj.print_x()  # Output: 5
